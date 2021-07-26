d604495f3402ccc49263363efc0bb29e4396b6cd
Jxian <jian1213783365@163.com>
2021-07-26 15:50:56 +0800

    first commit

---
+++ src/main/java/cn/itcast/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package cn.itcast.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+        System.out.println("good");
+        System.out.println("good2");
+        System.out.println("good3");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+    }
+}
